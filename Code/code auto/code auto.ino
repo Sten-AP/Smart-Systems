562540e1887dff8e131d4f365b5de662a1924b69
Sten-AP <s122441@ap.be>
2022-06-16 16:17:21 +0200

    code

--- Code/code auto/code auto.ino
+++ Code/code auto/code auto.ino
@@ -2,6 +2,26 @@
 #include <WiFi.h>              // Standard ESP32 Library
 #include <PubSubClient.h>      // https://github.com/knolleary/pubsubclient (intall with library Manager)
 
+<<<<<<< Updated upstream
+=======
+#define sub1 "Motor"
+#define sub2 "KeuzeMenu"
+
+const char* ssid = "brian";
+const char* password = "12345678";
+const char* mqtt_server = "mqtt.luytsm.be"; 
+
+const char* username = "";
+const char* pass = "";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE  (50)
+char msg[MSG_BUFFER_SIZE];
+int value = 0;
+
+>>>>>>> Stashed changes
 // PWM Settings - DO NOT CHANGE
 const int PWM_FREQUENCY = 500;
 const int PWM_RESOLUTION = 8;
@@ -35,6 +55,92 @@ int LCD_COLUMNS = 16;
 int LCD_ROWS = 2;
 LiquidCrystal_I2C lcd(0x27, LCD_COLUMNS, LCD_ROWS);
 
+<<<<<<< Updated upstream
+=======
+char keuzeMenu;
+char keuzeHandmatig;
+int drivingSpeed = 200; // default 128
+
+void setup_wifi()
+{
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length)
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  Serial.println();
+  if (strstr(topic, sub1))
+  {
+    for (int i = 0; i < length; i++)
+    {
+      Serial.println(char(payload[i]));
+      keuzeHandmatig = char(payload[i]);
+    }
+  }
+  else if (strstr(topic, sub2))
+  {
+    for (int i = 0; i < length; i++)
+    {
+      Serial.println(char(payload[i]));
+      keuzeMenu = char(payload[i]);
+    }
+  }
+  else
+  {
+    Serial.println("unsubscribed topic");
+  }
+}
+
+
+void reconnect()
+{
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str() , username, pass)) {
+      Serial.println();
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe(sub1);
+      client.subscribe(sub2);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+>>>>>>> Stashed changes
 enum COMMANDS {
   LEFT_TURN_FORWARD,      // 0
   FORWARD,                // 1
@@ -59,6 +165,7 @@ enum MOTOR_DIRECTION {
 };
 
 enum MENUKEUZE {
+<<<<<<< Updated upstream
   AUTOMATISCH,
   HANDMATIG,
   PARKOER
@@ -69,11 +176,26 @@ enum HANDMATIG {
   RECHTDOOR,
   RECHTS,
   ACHTERUIT
+=======
+  AUTOMATISCH = '8',
+  HANDMATIG = '9'
+};
+
+enum HANDMATIG {
+  LINKS = '0',
+  RECHTDOOR = '1',
+  RECHTS = '2',
+  ACHTERUIT = '3'
+>>>>>>> Stashed changes
 };
 
 int command, lastCommand;
 
+<<<<<<< Updated upstream
 int drivingSpeed = 128; // default 128
+=======
+int ledLinks, ledMidden, ledRechts;
+>>>>>>> Stashed changes
 
 void setup() {
   Serial.begin(115200);
@@ -99,6 +221,7 @@ void setup() {
   // lcd init 
   lcd.init();
   lcd.backlight();
+<<<<<<< Updated upstream
 }
 
 void loop() {
@@ -108,6 +231,28 @@ void loop() {
       int ledLinks = analogRead(linetrackerLed[0]);
       int ledMidden = analogRead(linetrackerLed[1]);
       int ledRechts = analogRead(linetrackerLed[2]);
+=======
+
+  setup_wifi();
+
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  switch (keuzeMenu) {
+    // Automatisch rijden van de wagen
+    Serial.println(keuzeMenu);
+    case AUTOMATISCH:
+      ledLinks = analogRead(linetrackerLed[0]);
+      ledMidden = analogRead(linetrackerLed[1]);
+      ledRechts = analogRead(linetrackerLed[2]);
+>>>>>>> Stashed changes
 
       Serial.println(String(ledLinks) + " - " + String(ledMidden) + " - " + String(ledRechts));
 
@@ -138,10 +283,13 @@ void loop() {
             break;
         }
       break;
+<<<<<<< Updated upstream
     // Voorgeprogrammeerd parkoer      
     case PARKOER:
       // ...
       break;
+=======
+>>>>>>> Stashed changes
     default:
       break;
   }
@@ -159,7 +307,11 @@ void loop() {
         delay(2000);
         break;
       case RIGHT: // Turn right around center point chassis
+<<<<<<< Updated upstream
         driveMotors(MOTOR_BACKWARDS, drivingSpeed, MOTOR_FORWARD, drivingSpeed);
+=======
+        driveMotors(MOTOR_BACKWARDS, drivingSpeed/2, MOTOR_FORWARD, drivingSpeed/2);
+>>>>>>> Stashed changes
         break;
       case RIGHT_TURN_BACKWARDS: // Curved turn backwards to the right
         driveMotors(MOTOR_BACKWARDS, drivingSpeed/2, MOTOR_BACKWARDS, drivingSpeed);
@@ -171,7 +323,11 @@ void loop() {
         driveMotors(MOTOR_BACKWARDS, drivingSpeed, MOTOR_BACKWARDS, drivingSpeed/2);
         break;
       case LEFT: // Turn left around center point chassis
+<<<<<<< Updated upstream
         driveMotors(MOTOR_FORWARD, drivingSpeed, MOTOR_BACKWARDS, drivingSpeed);
+=======
+        driveMotors(MOTOR_FORWARD, drivingSpeed/2, MOTOR_BACKWARDS, drivingSpeed/2);
+>>>>>>> Stashed changes
         break;
       case STOP: // Stop driving
         driveMotors(MOTOR_STOP, 0, MOTOR_STOP, 0);
